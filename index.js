4dc0f1f2738ed80798011b62568adc3359bb841b
Ana Beatriz dos Santos <99930108+anabeatwriz@users.noreply.github.com>
2024-02-11 15:30:55 -0300

    Bug fix 1 index.js

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ const perguntas = [
     const dt = quizItem.querySelector('dl dt').cloneNode(true)
     dt.querySelector('span').textContent = resposta
     dt.querySelector('input').setAttribute('name','pergunta-' + perguntas.indexOf(item))
-    dt.querySelector('input').value = item.respostas.indexOf(resposta)
+    dt.querySelector('input').value = resposta
     dt.querySelector("input").onchange = (event) => {
       const estaCorreta = event.target.value == item.correta
       corretas.delete(item)
