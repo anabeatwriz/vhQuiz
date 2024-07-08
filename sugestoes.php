07237af8ca02e913b8670fd99991345f37f187e0
Ana Beatriz dos Santos <99930108+anabeatwriz@users.noreply.github.com>
2024-07-08 01:45:45 +0000

    Ajuste na conexão com o DB

--- sugestoes.php
+++ sugestoes.php
@@ -7,42 +7,58 @@
     <link rel="stylesheet" href="css/cabeçalho.css">
     <link rel="stylesheet" href="css/rodape.css">
     <link rel="stylesheet" href="css/home-sug.css">
-    <link rel="shortcut icon" href="img/favicon.png" type="image">
-    <div class="conteudo">
-    <?php
-    include 'includes/cabeçalho.html';
-    ?>
-    </div>
+    <link rel="shortcut icon" href="img/favicon.png" type="image/png">
 </head>
 <body>
+    <div class="conteudo">
+        <?php include 'includes/cabeçalho.html'; ?>
+    </div>
 
-<div class="container">
-    <div class="sugestoes">
-        <div class="sug">
-            <h1>Deixe sua sugestão</h1>
-            <form action="sugestoes.php" method="post">
-                <input type="text" id="nome" placeholder="Qual é seu nome?" required>
-                <input type="email" id="email" placeholder="Digite seu e-mail" required>
-                <input type="text" id="sugcliente" placeholder="Descreva sua sugestão" required>
-                <h2>Avalie o vhQuiz</h2>
-                <div class="radio-group">
-                    <label><input type="radio" id="notainsatisfeito" name="nota" value="Insatisfeito"> Insatisfeito</label>
-                    <label><input type="radio" id="notaindiferente" name="nota" value="Neutro"> Neutro</label>
-                    <label><input type="radio" id="notasatisfeito" name="nota" value="Satisfeito"> Satisfeito</label>
-                </div>
-                <input type="submit" id="b.sug" value="Enviar minha sugestão">
-            </form>
-        </div>
-        <div class="sug-av">
-            <img src="img/conviteav.png" alt="Convite para avaliação">
+    <div class="container">
+        <div class="sugestoes">
+            <div class="sug">
+                <h1>Deixe sua sugestão</h1>
+                <form action="sugestoes.php" method="post">
+                    <input type="text" id="nome" name="nome" placeholder="Qual é seu nome?" required>
+                    <input type="email" id="email" name="email" placeholder="Digite seu e-mail" required>
+                    <input type="text" id="sugcliente" name="sugcliente" placeholder="Descreva sua sugestão" required>
+                    <h2>Avalie o vhQuiz</h2>
+                    <div class="radio-group">
+                        <label><input type="radio" id="notainsatisfeito" name="nota" value="Insatisfeito"> Insatisfeito</label>
+                        <label><input type="radio" id="notaindiferente" name="nota" value="Neutro"> Neutro</label>
+                        <label><input type="radio" id="notasatisfeito" name="nota" value="Satisfeito"> Satisfeito</label>
+                    </div>
+                    <input type="submit" id="b.sug" name="bsug" value="Enviar minha sugestão">
+                </form>
+            </div>
+            <div class="sug-av">
+                <img src="img/conviteav.png" alt="Convite para avaliação">
+            </div>
         </div>
     </div>
-</div>
-<div class="conteudo">
-    <?php
 
-    include 'includes/rodape.html';
+    <div class="conteudo">
+        <?php include 'includes/rodape.html'; ?>
+    </div>
+
+    <?php
+    if(isset($_POST['bsug'])){
+        include_once('conectar.php');
+
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $sugcliente = $_POST['sugcliente'];
+        $nota = $_POST['nota'];
+
+        $sql = "INSERT INTO vhquiz_prod (nome_usuario, email_usuario, sugestao_cliente, nota_cliente) VALUES ('$nome', '$email', '$sugcliente', '$nota')";
+        $usuarios = mysqli_query($conexao, $sql);
+
+        if ($usuarios) {
+            echo "Dados inseridos com sucesso!";
+        } else {
+            echo "Erro ao inserir os dados: " . $conexao->error;
+        }
+    }
     ?>
-</div>
 </body>
 </html>
